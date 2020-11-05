a1ae140a49fdbd7afaa25b36a3431a4dbdfae228
msenardi <matteo.s@docsity.com>
2020-11-05 17:54:43 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='sqlalchemy-connector',
-      version='0.1.18',
+      version='0.1.19',
       url='https://github.com/pualien/py-sqlalchemy-ssh-connector',
       license='MIT',
       author='Matteo Senardi',
