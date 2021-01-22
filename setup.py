0c75511e1fec1fb58a2c88eee5a8ccf2d2cf7adb
msenardi <matteo.s@docsity.com>
2021-01-22 14:44:43 +0100

    upgraded version to 0.1.21

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='sqlalchemy-connector',
-      version='0.1.21b',
+      version='0.1.21',
       url='https://github.com/pualien/py-sqlalchemy-ssh-connector',
       license='MIT',
       author='Matteo Senardi',
