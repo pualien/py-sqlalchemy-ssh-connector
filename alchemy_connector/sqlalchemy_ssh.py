9795c0ccf03cfa3c251369b5a62d630d36ffe9e6
msenardi <matteo.s@docsity.com>
2021-01-25 14:46:15 +0100

    cleaned code

--- alchemy_connector/sqlalchemy_ssh.py
+++ alchemy_connector/sqlalchemy_ssh.py
@@ -11,62 +11,49 @@ class SQLAlchemySession:
 
     def __init__(self, host=None, user=None, password=None, key=None, uri=None, port=22, to_host='127.0.0.1',
                  to_port=3306,
-                 data_map=None, local_bind_address_port=None):
+                 data_map=None, local_bind_address_port=None, use_ssh=True):
         if data_map is None:
             data_map = {}
         self.data_map = data_map
-        host = (data_map.get("ssh_host"), port) if data_map.get("ssh_host") else (host, port)
+        host = (data_map.get("ssh_host"), port) if self.data_map.get("ssh_host") is not None else (host, port)
         user = data_map.get("ssh_username") or user or getpass.getuser()
         key = data_map.get("ssh_host_key") or key or '/home/{user}/.ssh/id_rsa'.format(user=user)
         self.to_host = to_host
-        self.uri = parse.urlparse(data_map.get("connection_uri")) if data_map.get("connection_uri") else parse.urlparse(
+        self.uri = parse.urlparse(data_map.get("connection_uri")) if self.data_map.get("connection_uri") is not None else parse.urlparse(
             uri)
         self.engine = None
         self.connection = None
         self.db_url = None
         self.local_bind_address_port = local_bind_address_port
 
-        if uri:
+        if self.uri:
             to_host = self.uri.hostname or to_host
             to_port = self.uri.port or to_port
+        self.use_ssh = use_ssh
 
-        if password:
-            self.server = SSHTunnelForwarder(
-                host,
-                ssh_username=user,
-                ssh_password=password,
-                remote_bind_address=(to_host, to_port)
-            )
-        else:
-            self.server = SSHTunnelForwarder(
-                host,
-                ssh_username=user,
-                ssh_pkey=key,
-                remote_bind_address=(to_host, to_port)
-            )
+        if self.use_ssh:
+            if password:
+                self.server = SSHTunnelForwarder(
+                    host,
+                    ssh_username=user,
+                    ssh_password=password,
+                    remote_bind_address=(to_host, to_port)
+                )
+            else:
+                self.server = SSHTunnelForwarder(
+                    host,
+                    ssh_username=user,
+                    ssh_pkey=key,
+                    remote_bind_address=(to_host, to_port)
+                )
 
-        if self.local_bind_address_port is not None:
-            self.server.local_bind_address_port = self.local_bind_address_port
+            if self.local_bind_address_port is not None:
+                self.server.local_bind_address_port = self.local_bind_address_port
 
-        self.start()
+            self.start()
 
     def start(self):
         self.server.start()
-
-        params = dict()
-
-        if self.uri:
-
-            if '@' in self.uri.netloc:
-                user_pass = self.uri.netloc.split('@', 1)[0]
-                if ':' in user_pass:
-                    params['username'], params['password'] = user_pass.split(':', 1)
-                else:
-                    params['username'] = user_pass
-
-            if self.uri.query:
-                auth_mech = parse.parse_qs(self.uri.query)
-                params.update({key: auth_mech[key][0] for key in auth_mech})
 
         self.db_url = "{adapter}://{username}:{password}@localhost:{local_bind_port}/{database}".format(
             **self.data_map, local_bind_host=self.server.local_bind_host,
@@ -76,14 +63,16 @@ class SQLAlchemySession:
         self.connection = self.engine.connect()
 
     def stop(self):
-        self.connection.close()
-        self.server.stop(force=True)
-        self.connection = None
-        del self.connection
-        self.server = None
+        if self.use_ssh:
+            self.connection.close()
+            self.server.stop(force=True)
+            self.connection = None
+            del self.connection
+            self.server = None
 
     def close(self):
-        self.stop()
+        if self.use_ssh:
+            self.stop()
 
     def execute(self, query):
         """
